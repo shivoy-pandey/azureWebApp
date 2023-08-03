66b262e43b6635fdcfec53b33216df6c6b382f19
shivoy-pandey <117445896+shivoy-pandey@users.noreply.github.com>
2023-08-03 12:47:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Hello World From azure web app')
+from flask import Flask
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Define a route to handle incoming HTTP requests
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+# Start the Flask development server listening on port 8000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
